9d254d86f7b1df3f762f87a4b55cf6571b289e68
Unknown <argyelan@gmail.com>
2018-09-12 14:54:48 -0400

    These corrected some typo and Rscript version. REady to go, tested on TMS.

--- BIDSConverter/configfiles/overwrite_events.R
+++ BIDSConverter/configfiles/overwrite_events.R
@@ -9,10 +9,12 @@
   inp<-data.frame(inp)
   inp$V3[which(inp$V3==0)]<-'0-back'
   inp$V3[which(inp$V3==1)]<-'2-back'
-  tsv.files<-list.files(paste0('/output/sub-',subject,'/ses-',session,'/func/'),'task-nback_run-[0-9][0-9]_events.tsv',full.names = TRUE)
+  #print(subject)
+  #print(session)
+  tsv.files<-list.files(paste0('/output/sub-',subject,'/ses-',session,'/func'),'task-nback_run-[0-9][0-9]_events.tsv',full.names = TRUE)
   for (i in tsv.files)
   {
-  #    write.table( '\n', file=i, append = T, row.names = F, col.names = F, quote = F)
+      write.table( '\n', file=i, append = T, sep='\t',eol='', row.names = F, col.names = F, quote = F) #we need this to put the design in a new row
       write.table( inp,  file=i,  append = T, sep='\t', row.names=F, col.names=F,
                   quote = F)
   }
--- BIDSConverter/dicom2bids.sh
+++ BIDSConverter/dicom2bids.sh
@@ -9,5 +9,5 @@ if [ ${3} == "TMS" ]; then
 
   docker run -it --rm -v ${BIDS}/:/output -v /data/rTMS:/input/${1}:ro -v ${MY_FULL_PATH}:/heuristic:ro nipy/heudiconv -d /input/{subject}/{session}/dicom/*/* -s ${1} --ses ${2} -o /output -f /heuristic/configfiles/heuristicTMSv6.py  -b
   
-  docker run -it --rm -v ${MY_FULL_PATH}:/data -v ${BIDS}:/output --entrypoint=Rscript library/r-base /data/config_files/overwrite_events.R --vanilla ${1} ${2} #/data and /output should be left since it is in the overwrite_events.R 
+  docker run -it --rm -v ${MY_FULL_PATH}:/data -v ${BIDS}:/output --entrypoint=Rscript library/r-base --vanilla /data/configfiles/overwrite_events.R ${1} ${2} #/data and /output should be left since it is in the overwrite_events.R 
 fi
