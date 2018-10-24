67bff17a95e0ddd31789984f7b52957dfa767d3d
johncholewa <43482313+johncholewa@users.noreply.github.com>
2018-10-24 16:22:08 -0400

    Changed conversion so that study is autodiscovered

--- BIDSConverter/dicom2bids.sh
+++ BIDSConverter/dicom2bids.sh
@@ -10,20 +10,19 @@ TRASH=/analysis/.BIDSwork
 MY_PATH="`dirname \"$0\"`"
 MY_FULL_PATH=`readlink -f ${MY_PATH}`
 
-if [ ${3} == "TMS" ]; then
+# We should not need to specify a third parameter for study, because the study can be derived from the session path
+sessionpath="$(find_session ${2})"
+study="$(echo "${sessionpath}" | cut -d / -f 3)"
 
-  rawloc=`echo $(dirname $(/usr/local/bin/findsession $2))|grep TMS` 
-
-  docker run -i --rm -v ${rawloc}/${2}/dicom/:/input/dicom/${1}/${2}:ro -v ${BIDS}/:/output -v ${MY_FULL_PATH}:/derivatives -v ${TRASH}:/work  amiklos/bidskit:2.2 --indir=/input/dicom --outdir=/output #--overwrite
-  
-  docker run -i --rm -v ${MY_FULL_PATH}:/data -v ${BIDS}:/output --entrypoint=Rscript library/r-base --vanilla /data/configfiles/overwrite_events.R ${1} ${2} #/data and /output should be left since it is in the overwrite_events.R 
-fi
-
-
-if [ ${3} == "clozapineECT" ]; then
-
-  rawloc=`echo $(dirname $(/usr/local/bin/findsession $2))|grep Clozapine | head -n1`
-
-  docker run -i --rm -v ${rawloc}/${2}/dicom/:/input/dicom/${1}/${2}:ro -v ${BIDS}/:/output -v ${MY_FULL_PATH}:/derivatives -v ${TRASH}:/work  amiklos/bidskit:2.2 --indir=/input/dicom --outdir=/output #--overwrite
-  
-fi
+case $study in
+"rTMS")
+ docker run -i --rm -v ${sessionpath}/dicom/:/input/dicom/${1}/${2}:ro -v ${BIDS}/:/output -v ${MY_FULL_PATH}:/derivatives -v ${TRASH}:/work  amiklos/bidskit:2.2 --indir=/input/dicom --outdir=/output #--overwrite
+ docker run -i --rm -v ${MY_FULL_PATH}:/data -v ${BIDS}:/output --entrypoint=Rscript library/r-base --vanilla /data/configfiles/overwrite_events.R ${1} ${2} #/data and /output should be left since it is in the overwrite_events.R 
+ ;;
+"Clozapine")
+ docker run -i --rm -v ${sessionpath}/dicom/:/input/dicom/${1}/${2}:ro -v ${BIDS}/:/output -v ${MY_FULL_PATH}:/derivatives -v ${TRASH}:/work  amiklos/bidskit:2.2 --indir=/input/dicom --outdir=/output #--overwrite
+ ;;
+"BAMM")
+ docker run -i --rm -v ${sessionpath}/dicom/:/input/dicom/${1}/${2}:ro -v ${BIDS}/:/output -v ${MY_FULL_PATH}:/derivatives -v ${TRASH}:/work  amiklos/bidskit:2.2 --indir=/input/dicom --outdir=/output #--overwrite
+ ;;
+esac
