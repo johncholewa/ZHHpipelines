b26a334bd7a55431301ae05812fc50164d8f4d12
johncholewa <43482313+johncholewa@users.noreply.github.com>
2018-10-25 10:20:35 -0400

    Changed default BIDS destination
    In the future, we may have command-line opts for optionally setting values like this.

--- BIDSConverter/dicom2bids.sh
+++ BIDSConverter/dicom2bids.sh
@@ -5,8 +5,8 @@
 #OLD: using nipy/heudiconv which was pulled as latest on 2018-SEP-11
 # docker is called with -i (-t cant tolerated by SGEroot, and -d, would not be controlled by SGE)
 
-BIDS=/analysis/BIDS
-TRASH=/analysis/.BIDSwork
+BIDS=/data/BIDS
+TRASH=/data/.BIDSwork
 MY_PATH="`dirname \"$0\"`"
 MY_FULL_PATH=`readlink -f ${MY_PATH}`
 
@@ -14,7 +14,7 @@ MY_FULL_PATH=`readlink -f ${MY_PATH}`
 sessionpath="$(find_session ${2})"
 study="$(echo "${sessionpath}" | cut -d / -f 3)"
 
-case $study in
+case "$study" in
 "rTMS")
  docker run -i --rm -v ${sessionpath}/dicom/:/input/dicom/${1}/${2}:ro -v ${BIDS}/:/output -v ${MY_FULL_PATH}:/derivatives -v ${TRASH}:/work  amiklos/bidskit:2.2 --indir=/input/dicom --outdir=/output #--overwrite
  docker run -i --rm -v ${MY_FULL_PATH}:/data -v ${BIDS}:/output --entrypoint=Rscript library/r-base --vanilla /data/configfiles/overwrite_events.R ${1} ${2} #/data and /output should be left since it is in the overwrite_events.R 
