71124bff33a04fead87976dd13ab96d961d11a59
Unknown <argyelan@gmail.com>
2018-11-29 16:19:58 -0500

    new implementation makes ICA FIX possible

--- BIDSPipeline/HCPPipeline/bids_hcppipeline.sh
+++ BIDSPipeline/HCPPipeline/bids_hcppipeline.sh
@@ -8,7 +8,12 @@
 #example 2 with specific stages:
 #docker run -i --rm -v /analysis/BIDS/sub-10311:/mybids/sub-10311:ro -v /analysis/Argyelan/test:/output -v /nethome/amiklos/argyelan@gmail.com/howto/GitHub/hcppipeline_my:/mycoeff:ro -v /usr/local/opt/freesurfer5.3/:/mylicense:ro -v /analysis/BIDS/dataset_description.json:/mybids/dataset_description.json -v /analysis/BIDS/.bidsignore:/mybids/.bidsignore  amiklos/hcppipeline:4.04 --n_cpus 12 --gdcoeffs /mycoeff/coeff.grad --stages fMRIVolume fMRISurface --license_key /mylicense/license.txt /mybids /output participant
 
+#example 3 with ICAFIX
+#docker run -it --rm -v /analysis/BIDS/sub-10311:/mybids/sub-10311:ro -v /analysis/Argyelan/test:/output -v /nethome/amiklos/argyelan@gmail.com/howto/GitHub/hcppipeline_my:/mycoeff:ro -v /usr/local/opt/freesurfer5.3/:/mylicense:ro -v /analysis/BIDS/dataset_description.json:/mybids/dataset_description.json -v /analysis/BIDS/.bidsignore:/mybids/.bidsignore  amiklos/hcppipeline:4.10 --n_cpus 12 --gdcoeffs /mycoeff/coeff.grad --stages ICAFIX --license_key /mylicense/license.txt /mybids /output participant 
+
 
 #solve:/mycoeff/coeff.grad should be in the github as well...
 
 #amiklos/hcppipeline:4:04 is genearted based on argyelan/HCPPIPELINE repository ZHHSpecific branch
+
+
