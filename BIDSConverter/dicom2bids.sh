f55ef3632628be3305d838df8d41a5755c1a311f
Unknown <argyelan@gmail.com>
2019-01-03 15:03:47 -0500

    Auditory and inline_find_session
    This version now can deal with auditory task and even overwrites events file. Also move the find_session to an internal function, because I did not want to rely on anything specific that we have on the server and not in the repository.

--- BIDSConverter/dicom2bids.sh
+++ BIDSConverter/dicom2bids.sh
@@ -10,8 +10,14 @@ TRASH=/data/.BIDSwork
 MY_PATH="`dirname \"$0\"`"
 MY_FULL_PATH=`readlink -f ${MY_PATH}`
 
+inline_find_session(){
+  for study in 3T 611 BAMM rTMS FE Clozapine Clozapine-ECT SPINS tACS imagerepo; do
+    ls -1d /data/${study}/*${1} 2>/dev/null
+  done
+}
+
 # We should not need to specify a third parameter for study, because the study can be derived from the session path
-sessionpath="$(find_session ${2})"
+sessionpath="$(inline_find_session ${2})"
 study="$(echo "${sessionpath}" | cut -d / -f 3)"
 
 case "$study" in
@@ -27,5 +33,9 @@ case "$study" in
  ;;
 "BAMM")
  docker run -i --rm -v ${sessionpath}/dicom/:/input/dicom/${1}/${2}:ro -v ${BIDS}/:/output -v ${MY_FULL_PATH}:/derivatives -v ${TRASH}:/work  amiklos/bidskit:2.3 --indir=/input/dicom --outdir=/output #--overwrite
+ ;;
+"imagerepo")
+ docker run -i --rm -v ${sessionpath}/dicom/:/input/dicom/${1}/${2}:ro -v ${BIDS}/:/output -v ${MY_FULL_PATH}:/derivatives -v ${TRASH}:/work  amiklos/bidskit:2.3 --indir=/input/dicom --outdir=/output #--overwrite
+ docker run -i --rm -v ${MY_FULL_PATH}:/data -v ${BIDS}:/output --entrypoint=Rscript library/r-base --vanilla /data/configfiles/overwrite_events_auditory.R ${1} ${2} #/data and /output should be left since it is in the overwrite_events.R 
  ;;
 esac
