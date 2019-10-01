e534c8dc7cfbdd1b36743f4b05c89b7f8dcf679f
johncholewa <43482313+johncholewa@users.noreply.github.com>
2019-10-01 13:46:41 -0400

    Added SGAECT and IPER

--- BIDSConverter/dicom2bids.sh
+++ BIDSConverter/dicom2bids.sh
@@ -11,7 +11,7 @@ MY_PATH="`dirname \"$0\"`"
 MY_FULL_PATH=`readlink -f ${MY_PATH}`
 
 inline_find_session(){
-  for study in 3T 611 BAMM BORIS rTMS FE Clozapine Clozapine-ECT SPINS tACS imagerepo; do
+  for study in 3T 611 BAMM BORIS rTMS FE Clozapine Clozapine-ECT IPER SGAECT SPINS tACS imagerepo; do
     ls -1d /data/${study}/*${1} 2>/dev/null
   done
 }
